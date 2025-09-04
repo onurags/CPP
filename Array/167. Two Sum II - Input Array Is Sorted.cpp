9e86fbb140643b08d3207344f27d17180153016e
anurag <144724285+onurags@users.noreply.github.com>
2025-09-05 00:05:51 +0530

    167

--- Array/167. Two Sum II - Input Array Is Sorted.cpp
+++ Array/167. Two Sum II - Input Array Is Sorted.cpp
@@ -1,1 +1,20 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& arr, int target) {
+        int n = arr.size();
+        vector<int> ans;
+        int st = 0, end = n-1;
+        while(st<end){
+            if(arr[st] + arr[end] == target){
+                ans.push_back(st+1);
+                ans.push_back(end+1);
+                return ans;
+            }else if(arr[st] + arr[end] < target){
+                st++;
+            }else {
+                end--;
+            }
+        }
+        return ans;
+    }
+};
