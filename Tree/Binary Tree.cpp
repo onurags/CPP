db32b9a6d5491ffe8061eed63ea58872cb59ad54
anurag <144724285+onurags@users.noreply.github.com>
2025-09-08 23:35:36 +0530

    Create Binary Tree

---
+++ Tree/Binary Tree.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node *left, *right;
+    Node(int val){
+        data = val;
+        left = right = NULL;
+
+    }
+};
+
+Node * BinaryTree(){
+    int x;
+    cin>>x;
+    if(x == -1) return NULL;
+
+    Node * temp = new Node(x);
+    // left side
+    cout<<"Enter the left child of "<<x<<": ";
+    temp->left = BinaryTree();
+    // right side
+    cout<<"Enter the right child of "<<x<<": ";
+    temp->right = BinaryTree();
+    return temp;
+    
+}
+int main(){
+    cout<<"Enter the root node: ";
+    Node *root = BinaryTree();
+}
