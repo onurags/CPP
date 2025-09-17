91178ddc675ae07348890d2b93e40774a31d142e
anurag <144724285+onurags@users.noreply.github.com>
2025-09-18 00:34:31 +0530

    94

---
+++ Tree/94. Binary Tree Inorder Traversal.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void inOrder(TreeNode* root, vector<int> &ans){
+        if(!root) return;
+
+        inOrder(root->left, ans);
+        ans.push_back(root->val);
+        inOrder(root->right, ans);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        inOrder(root, ans);
+        return ans;
+    }
+};
